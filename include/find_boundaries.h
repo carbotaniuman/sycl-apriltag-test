4f2d5b54714cf7c8b1bda0b877688689a2941caa
carbotaniuman <41451839+carbotaniuman@users.noreply.github.com>
2025-01-08 22:58:49 -0800

    e

--- include/find_boundaries.h
+++ include/find_boundaries.h
@@ -6,7 +6,7 @@
 
 #include <sycl/sycl.hpp>
 
-sycl::event find_boundaries(sycl::queue &q, const uint32_t *labels,
+sycl::event find_boundaries(sycl::queue &q, const uint16_t *labels,
                             const HashTable::Entry *sizes, size_t sizes_elems,
                             BoundaryPoint *points, size_t width, size_t height,
                             const std::vector<sycl::event> &deps = {});
